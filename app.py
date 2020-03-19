0bf5c76f79c6d3cc3ad0304399402d51336ed76b
James Spurin <james@spurin.com>
2020-03-19 15:21:15 +0000

    Add hostname to output

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
+import socket
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    return '''You accessed path: /{}
-'''.format(path)
+    return '''Hostname: {}
+You accessed path: /{}
+'''.format(socket.gethostname(), path)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
